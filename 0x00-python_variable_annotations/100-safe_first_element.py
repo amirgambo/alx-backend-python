1cfacd9ea53a951edd51920a07ad86388162ae1a
amirgambo <gambo.amir@yahoo.com>
2023-10-05 13:21:21 +0000

    Variable Annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,10 @@
-from typing import Sequence, Any, Union
+#!/usr/bin/env python3
+"""Contains Augmented code with the correct duck-typed annotations."""
+from typing import Any, Union, Sequence
 
 
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of a list or None if the list is empty."""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,20 @@
-from typing import TypeVar, Mapping, Any, Union
+#!/usr/bin/env python3
+"""Contains method that safely gets value from dictionary."""
+from typing import Mapping, Any, Union, TypeVar, Optional
 
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: T = None) -> Union[Any, T]:
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """Safely gets value from dictionary.
+    Args:
+        dct (dict): Dictionary to get value from.
+        key (str): Key to get value from.
+        default (any): Default value to return if key is not found.
+    Returns:
+        Value from dictionary.
+    """
     if key in dct:
         return dct[key]
     else:
